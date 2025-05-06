8dd33ae6f7a309125c39763266ceb9728c9af6e4
silen7-pool <mo7trading@gmail.com>
2025-05-06 11:44:07 +0100

    rainbowkit wallet init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "@rainbow-me/rainbowkit/styles.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Web3Provider } from "./provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Web3Provider>
+      <App />
+    </Web3Provider>
+  </StrictMode>
+);
---
+++ src/provider.tsx
@@ -0,0 +1,23 @@
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { anvil, base } from "wagmi/chains";
+
+const config = getDefaultConfig({
+  appName: "Nom de ton app",
+  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+  chains: [base, anvil],
+  ssr: false,
+});
+
+const queryClient = new QueryClient();
+
+export function Web3Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
