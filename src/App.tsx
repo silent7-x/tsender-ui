da0d20c7ecbd2c9007fe9e2c71c6efe3e535ac08
silen7-pool <mo7trading@gmail.com>
2025-05-06 21:05:22 +0100

    adding shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
   return (
-    <main className="min-h-screen max-w-screen-xl mx-auto">
-      <ConnectButton />
+    <main className="min-h-screen max-w-screen-xl mx-auto pt-4 px-6 bg-amber-100">
+      <header className="flex justify-end">
+        <ConnectButton />
+      </header>
       <h1 className="">Hello World</h1>
     </main>
   );
--- src/provider.tsx
+++ src/provider.tsx
@@ -1,22 +1,39 @@
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { WagmiProvider } from "wagmi";
+import { useEffect, useState } from "react";
+import { http, WagmiProvider } from "wagmi";
 import { anvil, base } from "wagmi/chains";
 
+if (!import.meta.env.VITE_WALLETCONNECT_PROJECT_ID) {
+  throw new Error(
+    "Error: VITE_WALLETCONNECT_PROJECT_ID is not defined. Please set it in your .env.local file"
+  );
+}
+
 const config = getDefaultConfig({
-  appName: "Nom de ton app",
+  appName: "TSender",
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
   chains: [base, anvil],
+  transports: {
+    [base.id]: http(import.meta.env.VITE_BASE_ALCHEMY_RPC_URL),
+    [anvil.id]: http(import.meta.env.VITE_ANVIL_RPC_URL),
+  },
   ssr: false,
 });
 
 const queryClient = new QueryClient();
 
 export function Web3Provider({ children }: { children: React.ReactNode }) {
+  // Hydration safety check: ensure component mounts on client before rendering children
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>{children}</RainbowKitProvider>
+        <RainbowKitProvider>
+          {/* Only render children after client-side mounting */}
+          {mounted ? children : null}
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
